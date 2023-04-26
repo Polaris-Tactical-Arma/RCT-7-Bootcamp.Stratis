3424a533827fa83d255670c53ad9fd270d6153db
bapking <bapspam@live.co.uk>
2023-04-26 19:07:45 +0100

    wrote one page of intro text and removed a massive amount of bullshit

--- scarCODE/ServerInfoMenu/hpp/CfgServerInfoMenu.hpp
+++ scarCODE/ServerInfoMenu/hpp/CfgServerInfoMenu.hpp
@@ -13,104 +13,29 @@ class CfgServerInfoMenu
       class first
       {
          menuName = "General";
-         title = "<t color='#ffffff'>GENERAL INFORMATION</t>";
+         title = "<t color='#ffffff'>Welcome to the official RCT-7 bootcamp!</t>";
          content[] = {
-            "Welcome to Air Base Stratis, garrison of RCT-7! Have a look through this menu for all our community guidelines. Be sure to check out the 'Hits and Tips' tab for custom in game controls. Make sure to follow ALL rules as listed. The 'Index and FAQ' tab for info on featured areas on map to conducting a varity of training. For optimal game play, join our Discord. If you are enjoying the server, why not apply to join our Unit. See the 'Com's and MOD's' tab for links and instant access. If any problems or server issues, please contact an Admin either on our Discord or in game. Remember: Squad Up, Work Together, Communicate - Mission Complete! To accept our rules and terms of use - press Esc",
+            "Regimental Combat Team VII is a MilSim Unit and community established in 2017. We priortize having a good time and working together with an emphasis on understanding that real life comes first. 
+            <br />This bootcamp will demonstrate and test techniques utilised within our unit. Do not worry if you are not familiar, the bootcamp will explain everything you need.
+            <br />
+            <br /> You will be guided through the course by a series of tasks and hints - if at any point you're unsure of what you need to do, press 'J' on your keyboard to display your current tasks. 
+            <br />
+            <br />The bootcamp consists of 10 sections;
+            <br /><t size='.5'></t>1. Radio Familiarization
+            <br /><t size='.5'></t>2. Understanding 3D Reports
+            <br /><t size='.5'></t>3. Throwing Grenades
+            <br /><t size='.5'></t>4. Map Reading
+            <br /><t size='.5'></t>5. Close Quarters Combat
+            <br /><t size='.5'></t>6. Anti-Tank Usage
+            <br /><t size='.5'></t>7. Anti-Air Usage
+            <br /><t size='.5'></t>8. Formations
+            <br /><t size='.5'></t>9. Bounding
+            <br /><t size='.5'></t>10. ACE Medical
+            <br />
+            <br />Your progress is saved on completion of each of these sections - you will resume at correct point if you need to reconnect.
+            <br />
+            <br />If you experience any issues please ping a @Recruiter in Discord for assistance.",
 			"<br />",
-         };
-      };
-      class second
-      {
-         menuName = "Rules";
-         title = "SERVER RULES";
-         content[] = {
-            "<t size='1.25'>Do Not Break - Follow Always!</t><br />",
-			"<t size='1.25'></t><br />#1 Players/Admins are NEVER permitted to show racism and disrespect towards any other player/admin at any time.<br />",
-			"<t size='1.25'></t><br />#2 No Team Killing - This map is NOT for PvP.<br />",
-			"<t size='1.25'></t><br />#3 No Trolling/Griefing Players - Whats funny to you isn't funny to all.<br />",
-			"<t size='1.25'></t><br />#4 Never Misuse Assets - Doing so will result in an automatic kick and possible escalation of discipline.<br />",
-			"<t size='1.25'></t><br />#5 No Discharging Firearms Randomly - Use designated areas (Firing Ranges, MOUT, Ordnace Ranges, etc) for live fire.<br />",
-			"<t size='1.25'></t><br />#6 Never Intentionally Destroy Assets - Doing so will result in an automatic kick and possible escalation of discipline.<br />",
-			"<t size='1.25'></t><br />#7 You may test out all assets and areas placed/located on map.<br />",
-			"<t size='1.25'></t><br />#8 Accidents Happen - If you do any of the aformentioned 'Donts' by mistake, simple report to an Admin (if there are online).<br />",
-			"<t size='1.25'></t><br />#9 If a player is breaking any of the afotmentioned 'Donts' and is not heeding verbal warnings, take their player name and report it to an Admin at once!<br />",
-			"<t size='1.25'></t><br />#10 Do NOT Mic Spam/Text - Be polite when asking questions.<br />",
-			"<t size='1.25'></t><br />#11 If you ask questions that are answered in this menu, you will be asked to reconnect and read through it. If you persist - you will be kicked.<br />",
-			"<t size='1.25'></t><br />#12 If you think the punishment is given unfair or you have been missed judged, please contact an Admin for further adjudication.<br />",
-			"<br />",
-
-         };
-      };
-	  class three
-      {
-         menuName = "Index & FAQ";
-         title = "Frequently Asked Questions";
-         content[] = { 
-			"<t size='1.25'>Note:</t><br />this is a shared training map for all sub-units of RCT-7. Be considerate of others when using shared spaces. Professionalism and respect go a long way!<br />",
-			"<br />",
-			"<t size='1.25'>--------Live Fire Ranges--------</t><br /><br />",
-			"<t size='1.25'></t><br />-There are 3 firing ranges featured on this map; one on post and two at Kamino Firing Range. All are interactive and set up for differnt kinds of shoots and training.<br />",
-			"<t size='1.25'></t><br />-The 'Vehicle Range' provides terrian obstacles, pop up/stationary targets and 3 distinct shooting positions to practice from.<br />",
-			"<t size='1.25'></t><br />-The 'MOUT Training Area' features a realistic mock up of a urban environment to practice CQB methods while filled with both agressive/passive targets to be PID and engaged.<br />",
-			"<t size='1.25'></t><br />-There are 3 ordnace ranges that provide mobile and static naval tagets for rotary/fixed wing assets to engage.<br />",
-			"<br />",
-			"<t size='1.35'></t>--------The Fleet--------<br /><br />",
-			"<t size='1.25'></t><br />-There are 6 ships that make up the Fleet RCT-7 deploys with, 4 static and 1 fully operational.<br />",
-			"<t size='1.25'></t><br />-The USS Freedom (CVN-83) and USS Khe Sanh (LHD-9) provide full 'Service Centers' for rotary/fixed wing assets. The USS Liberty (DDG-1001) and USS Everett (LPD-29) can only provide rearm/refuel and not refit/pylon changes.<br />",
-			"<t size='1.25'></t><br />-The USS Jimmy Carter (SSN-23) provides NSW capabilites with the deployment of a SVD she carries.<br />",
-			"<br />",
-			"<t size='1.35'>--------Moving Across Post--------</t><br /><br />",
-			"<t size='1.25'></t><br />-For general travel hop into a M1123(2D/Open/Snorkel) and drive to your intended location. Deserting the vehicle at 5m will force it to respwan where you got it from.<br />",
-			"<t size='1.25'></t><br />-6 teleporting stations are available for travel between certian areas on the map. They are identifiable by the a Sign (Military Area, English).<br />",
-		 
-		 
-		 };
-      };
-	  
-      class fifth
-      {
-         menuName = "Hits & Tips";
-         title = "Custom Controls";
-         content[] = { 
-			 "<t size='1.25'>To Squad Up -</t> Press ace3 'Interaction' key",
-			 "<br />",
-			 "<t size='1.25'>To Insert Ear Plugs -</t> Press ace3 'Self Interation' key",
-			 "<br />",
-			 "<t size='1.25'>To Holster Weapons -</t> Press '0' key",
-			 "<br />",
-			 "<t size='1.25'>To Entrench -</t> Press ace3 'Self Interaction' key",
-			 "<br />",
-			 "<t size='1.25'>To Repack Mags -</t> Double left click 'Partial Mags' in Inventory",
-			  "<br />",
-			 "<t size='1.25'>To Drag Wounded -</t> Press ace3 'Interaction' key",
-			  "<br />",
-			 "<t size='1.25'>To Release Wounded -</t> Press ace3 'Interaction' key",
-			 "<br />",
-			 "<t size='1.25'>To Strap IR Strobe/Glow Sticks -</t> Press ace3 'Self Interaction' key"
-		 
-		 
-		 };
-      };
-      class fourth 
-      {
-         menuName = "Com's & MOD's";
-         title = "VOICE-SERVER-WEBSITE-MOD's";
-         content[] = {
-
-            "<a href='https://discord.gg/QxTeJpa'><t size='1.25'>Join Our Discord!</a>",
-            "<br />",
-            "<a href='https://units.arma3.com/unit/rct7'><t size='1.25'>Join Our Team!</a>",
-			"<br />",
-			"<a href='https://steamcommunity.com/sharedfiles/filedetails/?id=1499161380'><t size='1.25'>Get Our Mod's!</a>"
-		 };
-      };
-      class seventh
-      {
-         menuName = "Credits";
-         title = "CREDITS WHERE CREDITS DUE";
-         content[] = {
-            "<t size='1.25'>RCT-7 Dev Team:</t> Shhh you will never know.... unless you join us *winky face* ",
-            "<br />",
          };
       };
    };
